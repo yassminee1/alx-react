e0bb36d2362f4d7f20086e9eed90c9cadd0ec640
yassminee <142147424+yassminee00@users.noreply.github.com>
2024-06-06 14:33:11 +0100

    Update notifications.js

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -2,7 +2,7 @@ import notificationData from '../../../../notifications.json';
 import { normalize, schema } from 'normalizr';
 
 const user = new schema.Entity('users');
-const message = new schema.Entity('messages', {}, { idAttribute: 'guide' });
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
 const notification = new schema.Entity('notification', {
 		author: user,
 		context: message,
