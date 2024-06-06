58927073b7349050d3bc45f973c55fa94c3e4443
yassminee <142147424+yassminee00@users.noreply.github.com>
2024-06-06 13:13:12 +0100

    Update notifications.js

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,4 +1,4 @@
-mport * as notificationData from "../../../../notifications.json";
+import * as notificationData from "../../../../notifications.json";
 
 export default function getAllNotificationsByUser(userId) {
   return notificationData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
